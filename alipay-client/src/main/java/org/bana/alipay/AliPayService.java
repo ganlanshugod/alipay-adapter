453b45a5fa976e3acb89435bc4ac2184ec2ad5cb
chulinpeng <chulinpeng@elink-cn.com>
2020-05-26 14:52:43 +0800

    ##封装支付宝接口

--- alipay-client/src/main/java/org/bana/alipay/AliPayService.java
+++ alipay-client/src/main/java/org/bana/alipay/AliPayService.java
@@ -8,6 +8,8 @@
 */
 package org.bana.alipay;
 
+import java.util.Map;
+
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayRequest;
 import com.alipay.api.AlipayResponse;
@@ -18,9 +20,76 @@ import com.alipay.api.AlipayResponse;
  * @author ChuLinpeng
  */
 public interface AliPayService {
+	
+	/**
+     * 
+     * 
+     * @param <T>
+     * @param request
+     * @return
+     * @throws AlipayApiException
+     */
+    public <T extends AlipayResponse> T execute(String appId, AlipayRequest<T> request) throws AlipayApiException;
 
-	public <T extends AlipayResponse> T execute(String appId, AlipayRequest<T> request) throws AlipayApiException;
+    /**
+     * 
+     * 
+     * @param <T>
+     * @param request
+     * @param accessToken
+     * @return
+     * @throws AlipayApiException
+     */
+    public <T extends AlipayResponse> T execute(String appId, AlipayRequest<T> request,
+                                                String authToken) throws AlipayApiException;
 
-	public <T extends AlipayResponse> T execute(String appId, AlipayRequest<T> request, String authToken)
-			throws AlipayApiException;
+    /**
+     * 
+     * 
+     * @param request
+     * @param accessToken
+     * @param appAuthToken
+     * @return
+     * @throws AlipayApiException
+     */
+    public <T extends AlipayResponse> T execute(String appId, AlipayRequest<T> request, String accessToken,
+                                                String appAuthToken) throws AlipayApiException;
+
+    /**
+     * 
+     * @param <T>
+     * @param request
+     * @return
+     * @throws AlipayApiException
+     */
+    public <T extends AlipayResponse> T pageExecute(String appId, AlipayRequest<T> request) throws AlipayApiException;
+    
+    /**
+     * SDK客户端调用生成sdk字符串
+     * @param <T>
+     * @param request
+     * @return
+     * @throws AlipayApiException
+     */
+    public <T extends AlipayResponse> T sdkExecute(String appId, AlipayRequest<T> request) throws AlipayApiException;
+
+    /**
+    * 
+    * @param request
+    * @return
+    * @throws AlipayApiException
+    */
+    public <T extends AlipayResponse> T pageExecute(String appId, AlipayRequest<T> request,
+                                                    String method) throws AlipayApiException;
+
+    /**
+     * 移动客户端同步结果返回解析的参考工具方法
+     * 
+     * @param result 移动客户端SDK同步返回的结果map，一般包含resultStatus，result和memo三个key
+     * @param requsetClazz 接口请求request类，如App支付传入 AlipayTradeAppPayRequest.class
+     * @return 同步返回结果的response对象
+     * @throws AlipayApiException
+     */
+    public <TR extends AlipayResponse, T extends AlipayRequest<TR>> TR parseAppSyncResult(String appId, Map<String, String> result,
+                                                                                          Class<T> requsetClazz) throws AlipayApiException;
 }
--- alipay-client/src/main/java/org/bana/alipay/impl/AlipayServiceImpl.java
+++ alipay-client/src/main/java/org/bana/alipay/impl/AlipayServiceImpl.java
@@ -5,8 +5,10 @@
 * @author ChuLinpeng   
 * @date 2020年5月26日 下午1:40:32 
 * @version V1.0   
-*/ 
+*/
 package org.bana.alipay.impl;
+
+import java.util.Map;
 
 import org.bana.alipay.AliPayService;
 import org.bana.alipay.AlipayClientFactory;
@@ -15,44 +17,155 @@ import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayRequest;
 import com.alipay.api.AlipayResponse;
 
-/** 
-* @ClassName: AlipayServiceImpl 
-* @Description: 
-* @author ChuLinpeng   
-*/
+/**
+ * @ClassName: AlipayServiceImpl
+ * @Description:
+ * @author ChuLinpeng
+ */
 public class AlipayServiceImpl implements AliPayService {
-	
+
 	private AlipayClientFactory alipayClientFactory;
-	
+
 	/**
-	* <p>Description: </p> 
-	* @author ChuLinpeng   
-	* @date 2020年5月26日 下午1:44:54 
-	* @param appId
-	* @param request
-	* @return
-	* @throws AlipayApiException 
-	* @see org.bana.alipay.AliPayService#execute(java.lang.String, com.alipay.api.AlipayRequest) 
-	*/ 
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @author ChuLinpeng
+	 * @date 2020年5月26日 下午1:44:54
+	 * @param appId
+	 * @param request
+	 * @return
+	 * @throws AlipayApiException
+	 * @see org.bana.alipay.AliPayService#execute(java.lang.String,
+	 *      com.alipay.api.AlipayRequest)
+	 */
 	@Override
 	public <T extends AlipayResponse> T execute(String appId, AlipayRequest<T> request) throws AlipayApiException {
 		return alipayClientFactory.getAlipayClient(appId).execute(request);
 	}
-	
+
 	/**
-	* <p>Description: </p> 
-	* @author ChuLinpeng   
-	* @date 2020年5月26日 下午1:43:14 
-	* @param appId
-	* @param request
-	* @param authToken
-	* @return
-	* @throws AlipayApiException 
-	* @see org.bana.alipay.AliPayService#execute(java.lang.String, com.alipay.api.AlipayRequest, java.lang.String) 
-	*/ 
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @author ChuLinpeng
+	 * @date 2020年5月26日 下午1:43:14
+	 * @param appId
+	 * @param request
+	 * @param authToken
+	 * @return
+	 * @throws AlipayApiException
+	 * @see org.bana.alipay.AliPayService#execute(java.lang.String,
+	 *      com.alipay.api.AlipayRequest, java.lang.String)
+	 */
 	@Override
-	public <T extends AlipayResponse> T execute(String appId, AlipayRequest<T> request, String authToken) throws AlipayApiException {
+	public <T extends AlipayResponse> T execute(String appId, AlipayRequest<T> request, String authToken)
+			throws AlipayApiException {
 		return alipayClientFactory.getAlipayClient(appId).execute(request, authToken);
+	}
+
+	/**
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @author ChuLinpeng
+	 * @date 2020年5月26日 下午2:46:11
+	 * @param appId
+	 * @param request
+	 * @param accessToken
+	 * @param appAuthToken
+	 * @return
+	 * @throws AlipayApiException
+	 * @see org.bana.alipay.AliPayService#execute(java.lang.String,
+	 *      com.alipay.api.AlipayRequest, java.lang.String, java.lang.String)
+	 */
+	@Override
+	public <T extends AlipayResponse> T execute(String appId, AlipayRequest<T> request, String accessToken,
+			String appAuthToken) throws AlipayApiException {
+		return alipayClientFactory.getAlipayClient(appId).execute(request, accessToken, appAuthToken);
+	}
+
+	/**
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @author ChuLinpeng
+	 * @date 2020年5月26日 下午2:46:11
+	 * @param appId
+	 * @param request
+	 * @return
+	 * @throws AlipayApiException
+	 * @see org.bana.alipay.AliPayService#pageExecute(java.lang.String,
+	 *      com.alipay.api.AlipayRequest)
+	 */
+	@Override
+	public <T extends AlipayResponse> T pageExecute(String appId, AlipayRequest<T> request) throws AlipayApiException {
+		return alipayClientFactory.getAlipayClient(appId).pageExecute(request);
+	}
+
+	/**
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @author ChuLinpeng
+	 * @date 2020年5月26日 下午2:46:11
+	 * @param appId
+	 * @param request
+	 * @return
+	 * @throws AlipayApiException
+	 * @see org.bana.alipay.AliPayService#sdkExecute(java.lang.String,
+	 *      com.alipay.api.AlipayRequest)
+	 */
+	@Override
+	public <T extends AlipayResponse> T sdkExecute(String appId, AlipayRequest<T> request) throws AlipayApiException {
+		return alipayClientFactory.getAlipayClient(appId).sdkExecute(request);
+	}
+
+	/**
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @author ChuLinpeng
+	 * @date 2020年5月26日 下午2:46:11
+	 * @param appId
+	 * @param request
+	 * @param method
+	 * @return
+	 * @throws AlipayApiException
+	 * @see org.bana.alipay.AliPayService#pageExecute(java.lang.String,
+	 *      com.alipay.api.AlipayRequest, java.lang.String)
+	 */
+	@Override
+	public <T extends AlipayResponse> T pageExecute(String appId, AlipayRequest<T> request, String method)
+			throws AlipayApiException {
+		return alipayClientFactory.getAlipayClient(appId).pageExecute(request, method);
+	}
+
+	/**
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @author ChuLinpeng
+	 * @date 2020年5月26日 下午2:46:11
+	 * @param appId
+	 * @param result
+	 * @param requsetClazz
+	 * @return
+	 * @throws AlipayApiException
+	 * @see org.bana.alipay.AliPayService#parseAppSyncResult(java.lang.String,
+	 *      java.util.Map, java.lang.Class)
+	 */
+	@Override
+	public <TR extends AlipayResponse, T extends AlipayRequest<TR>> TR parseAppSyncResult(String appId,
+			Map<String, String> result, Class<T> requsetClazz) throws AlipayApiException {
+		return alipayClientFactory.getAlipayClient(appId).parseAppSyncResult(result, requsetClazz);
 	}
 
 	public void setAlipayClientFactory(AlipayClientFactory alipayClientFactory) {
