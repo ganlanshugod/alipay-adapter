397f7dfb965a2728c0a1b86bc42c5a9ec9fef19f
chulinpeng <chulinpeng@elink-cn.com>
2020-09-17 17:19:37 +0800

    ##增加AlipayClient在内存中的有效时间参数

--- alipay-client/src/main/java/org/bana/alipay/impl/DefaultAlipayClientCache.java
+++ alipay-client/src/main/java/org/bana/alipay/impl/DefaultAlipayClientCache.java
@@ -8,10 +8,13 @@
 */ 
 package org.bana.alipay.impl;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.bana.alipay.AlipayClientCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.alipay.api.AlipayClient;
 
@@ -21,7 +24,29 @@ import com.alipay.api.AlipayClient;
 * @author ChuLinpeng   
 */
 public class DefaultAlipayClientCache implements AlipayClientCache {
-	private Map<String, AlipayClient> cacheMap = new HashMap<String, AlipayClient>();
+	private static final Logger LOG = LoggerFactory.getLogger(DefaultAlipayClientCache.class);
+	private static final String CLIENT_KEY = "clicnt";
+	private static final String DATE_KEY = "date";
+	// AlipayClient在内存中的存储有效时间，默认1天
+	private int clientEffectiveTime = 24 * 60 * 60 * 1000;
+	private Map<String, Map<String, Object>> cacheMap = new HashMap<String, Map<String, Object>>();
+	
+	public DefaultAlipayClientCache() {
+		super();
+	}
+	
+	/** 
+	* <p>Description: </p> 
+	* @author ChuLinpeng   
+	* @date 2020年9月17日 下午5:03:53  
+	*/
+	public DefaultAlipayClientCache(Integer clientEffectiveTime) {
+		super();
+		if (clientEffectiveTime != null && clientEffectiveTime.intValue() > 0) {
+			this.clientEffectiveTime = clientEffectiveTime;
+		}
+		LOG.info("当前AlipayClient在内存中的有效时间为：" + clientEffectiveTime + "毫秒");
+	}
 
 	/**
 	* <p>Description: </p> 
@@ -33,7 +58,10 @@ public class DefaultAlipayClientCache implements AlipayClientCache {
 	*/ 
 	@Override
 	public void put(String appId, AlipayClient client) {
-		cacheMap.put(appId, client);
+		Map<String, Object> valMap = new HashMap<String, Object>();
+		valMap.put(DATE_KEY, new Date()); // 当前时间，校验有效期用
+		valMap.put(CLIENT_KEY, client);
+		cacheMap.put(appId, valMap);
 	}
 
 	/**
@@ -46,7 +74,14 @@ public class DefaultAlipayClientCache implements AlipayClientCache {
 	*/ 
 	@Override
 	public AlipayClient get(String appId) {
-		return cacheMap.get(appId);
+		Map<String, Object> valMap = cacheMap.get(appId);
+		Date saveDate = (Date)valMap.get(DATE_KEY);
+		Date now = new Date();
+		// 若超过有效时间，则返回null
+		if (now.getTime() - saveDate.getTime() > clientEffectiveTime) {
+			return null;
+		}
+		return (AlipayClient)valMap.get(CLIENT_KEY);
 	}
 
 }
